251a1a3b320a3ee034c7f1556375b041546c8f68
student203 <student203@203-06>
2023-02-27 10:38:07 +0500

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,18 @@
+namespace pril
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            int c = a + b;
+            label1.Text = c.ToString();
+        }
+    }
+}
